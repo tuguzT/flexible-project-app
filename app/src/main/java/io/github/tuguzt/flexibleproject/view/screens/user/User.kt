aa8e2e72065ec40c2cc7d29663ae7fc11fc33edd
tuguzT <timurka.tugushev@gmail.com>
2023-05-08 10:38:03 +0300

    Add top bar on user screen

--- app/src/main/java/io/github/tuguzt/flexibleproject/view/screens/user/User.kt
+++ app/src/main/java/io/github/tuguzt/flexibleproject/view/screens/user/User.kt
@@ -1,15 +1,37 @@
 package io.github.tuguzt.flexibleproject.view.screens.user
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
-import io.github.tuguzt.flexibleproject.R
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import io.github.tuguzt.flexibleproject.viewmodel.user.UserViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph
 @Destination
 @Composable
-fun UserScreen() {
-    Text(text = stringResource(R.string.user))
+fun UserScreen(
+    navigator: DestinationsNavigator,
+    viewModel: UserViewModel = hiltViewModel(),
+) {
+    val state by viewModel.stateFlow.collectAsState()
+
+    Scaffold(
+        topBar = {
+            UserTopBar(
+                user = state.user ?: return@Scaffold,
+                onNavigationClick = navigator::navigateUp,
+            )
+        },
+    ) { padding ->
+        Box(modifier = Modifier.padding(padding))
+    }
 }
