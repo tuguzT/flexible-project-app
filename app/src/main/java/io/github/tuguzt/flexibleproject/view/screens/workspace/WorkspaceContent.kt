6db852839ea5ed8d71907ddfbe816d6a20ab5d0e
tuguzT <timurka.tugushev@gmail.com>
2023-06-02 23:34:17 +0300

    Show workspace properties on workspace screen

--- app/src/main/java/io/github/tuguzt/flexibleproject/view/screens/workspace/WorkspaceContent.kt
+++ app/src/main/java/io/github/tuguzt/flexibleproject/view/screens/workspace/WorkspaceContent.kt
@@ -1,12 +1,27 @@
 package io.github.tuguzt.flexibleproject.view.screens.workspace
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Description
+import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.placeholder.material.placeholder
 import io.github.tuguzt.flexibleproject.domain.model.workspace.Visibility
 import io.github.tuguzt.flexibleproject.domain.model.workspace.Workspace
 import io.github.tuguzt.flexibleproject.domain.model.workspace.WorkspaceData
@@ -30,8 +45,83 @@ fun WorkspaceContent(
             )
         },
     ) { padding ->
-        Box(modifier = Modifier.padding(padding))
-        // TODO add some content
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+        ) {
+            WorkspaceDataCard(
+                data = workspace?.data,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun WorkspaceDataCard(
+    data: WorkspaceData?,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier,
+        tonalElevation = 1.dp,
+    ) {
+        Column {
+            WorkspaceDescription(
+                description = data?.description,
+                modifier = Modifier.padding(16.dp),
+            )
+            WorkspaceVisibility(
+                visibility = data?.visibility,
+                modifier = Modifier.padding(16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun WorkspaceDescription(
+    description: String?,
+    modifier: Modifier = Modifier,
+) {
+    WorkspaceItemRow(
+        data = description,
+        icon = Icons.Rounded.Description,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun WorkspaceVisibility(
+    visibility: Visibility?,
+    modifier: Modifier = Modifier,
+) {
+    WorkspaceItemRow(
+        data = visibility?.toString(),
+        icon = Icons.Rounded.Visibility,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun WorkspaceItemRow(
+    data: String?,
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    iconDescription: String? = null,
+    placeholder: String = "placeholder",
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.Top,
+    ) {
+        Icon(imageVector = icon, contentDescription = iconDescription)
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = data ?: placeholder,
+            modifier = Modifier.placeholder(visible = data == null),
+        )
     }
 }
 
@@ -42,7 +132,10 @@ private fun WorkspaceContentWithWorkspace() {
         id = WorkspaceId("workspace"),
         data = WorkspaceData(
             name = "Some workspace",
-            description = "",
+            description = """
+                Very long description
+                It is so long because I need to test the screen content
+            """.trimIndent(),
             visibility = Visibility.Public,
             image = null,
         ),
